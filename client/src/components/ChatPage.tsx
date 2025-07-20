8cb7ad63c49eca0d61d49b304a71eb4a0b966b33
Aaryan10-sudo <lazyfox916@gmail.com>
2025-07-20 22:15:20 +0545

    LangChain

--- client/src/components/ChatPage.tsx
+++ client/src/components/ChatPage.tsx
@@ -18,7 +18,7 @@ export default function ChatPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/chat", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
